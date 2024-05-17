2e1701bd9be17d1af6447da8caca999985a3ffe6
DeshonS <deshonjewell@gmail.com>
2024-05-17 22:26:53 +0000

    first attempt at 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'prints a string'
+ * Return: Always 0 (Success)
+ */
+
+main(){
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
