a40569da463c3964ddbebf1e3640b2c2ef8cfcb6
DeshonS <deshonjewell@gmail.com>
2024-05-19 00:34:11 +0000

    first attempt at 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print all digits of base 10 in order'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 9; x++)
+	printf("%i", x);
+	return (0);
+}
