57b494298175d395b140554fba4cc5eacd059ba6
DeshonS <deshonjewell@gmail.com>
2024-05-19 00:44:38 +0000

    updated

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-        int x;
+	int x;
 
-        for (x = 0; x <= 9; x++)
-        {
-        putchar('0' + x);
-        }
-        putchar('\n');
-        return (0);
+	for (x = 0; x <= 9; x++)
+	{
+	putchar('0' + x);
+	}
+	putchar('\n');
+	return (0);
 }
