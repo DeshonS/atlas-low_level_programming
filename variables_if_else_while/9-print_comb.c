b09a137fb21898f25a90354ffd2c56c42057d5aa
DeshonS <deshonjewell@gmail.com>
2024-05-19 01:06:32 +0000

    first try at 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'prints all single digit numbers separated by commas'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	if (x > 0)
+	{
+	putchar(", ");
+	putchar('0' + x);
+	}
+	else
+	{
+	putchar('0' + x);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
