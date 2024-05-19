492b1817251cb822a1410b280f13df517657eaa5
DeshonS <deshonjewell@gmail.com>
2024-05-19 00:39:17 +0000

    first attempt at 6, i will laugh if this works first try

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print all digits of base 10 in order'
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int x;
+
+        for (x = 0; x <= 9; x++)
+        {
+        putchar(x);
+        }
+        putchar('\n');
+        return (0);
+}
