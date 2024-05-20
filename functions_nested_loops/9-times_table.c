b06bc50e4a5e1cde574346600fde7a967919e4fb
DeshonS <deshonjewell@gmail.com>
2024-05-20 13:41:55 +0000

    first attempt at 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - opening of file
+ * Description - displays times tables
+ * Return - Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int x;
+	int y;
+	x = 0;
+
+	while (x < 10)
+	{
+		y = 0;
+
+		while (y < 10)
+		{
+		z = (x * y);
+		_putchar(z);
+		_putchar(',');
+		_putchar(' ');
+		y++;
+		}
+	_putchar('\n');
+	x++;
+	}
+	return (0);
+}
