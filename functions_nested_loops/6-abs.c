0c913285d0b8375e927bb8fc904264c89af795b3
DeshonS <deshonjewell@gmail.com>
2024-05-20 11:49:43 +0000

    first attempt at 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - opening of file
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+
+int _abs(int r)
+{
+	if (r > 0)
+	{
+	return (r);
+	}
+	else
+	{
+	return (-r);
+	}
+}
