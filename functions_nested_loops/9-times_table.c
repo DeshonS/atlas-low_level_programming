76d1a1ee120c61d323270a28ca716d0bb307d4e0
DeshonS <deshonjewell@gmail.com>
2024-05-20 22:12:17 +0000

    rewrote the script

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,27 +8,20 @@
 
 void times_table(void)
 {
-	int x;
-	int y;
-	x = 0;
-
-	while (x < 10)
+	for (x = 0; x <= 10; x++)
 	{
-		y = 0;
-
-		while (y < 10)
+		for (y = 0; y <= 10; y++);
 		{
-		int z;	
-		z = (x * y);
-		_putchar(z);
-		if (y == 9)
-			continue;
+			int z;
+			z = x * y;
+			_putchar(z);
+			if (y == 9)
+				continue;
 
-		_putchar(',');
-		_putchar(' ');
-		y++;
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
 		}
-	_putchar('\n');
-	x++;
 	}
 }
+
