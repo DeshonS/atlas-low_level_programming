01e3e3aa17b7b32f4d759d51913ab268274a4c95
DeshonS <deshonjewell@gmail.com>
2024-05-20 22:44:39 +0000

    first attempt at 10;

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * add - opening of file
+ * Description - adds two numbers and prints result
+ * Return - Always 0 (Success)
+ */
+
+int add(int, int)
+{
+	int n;
+	int o;
+	int p;
+
+	p = n + o;
+	_putchar(p);
+	return (0);
+}
