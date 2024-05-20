4f95b62e4e99016a9c90e8b153801601628fb6d6
DeshonS <deshonjewell@gmail.com>
2024-05-20 22:58:51 +0000

    first attempt at 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints from an integer to 98
+ * @n - variable integer to be printed from
+ * Return: always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+	_putchar(n);
+	n++;
+	}
+}
