dd689f3bd718204309285cdafe97c943a1dd5055
DeshonS <deshonjewell@gmail.com>
2024-05-23 22:06:00 +0000

    first attempt at 3

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - opening of file
+ * @a: an integer to be multiplied with b
+ * @b: an integer to be multiplied with a
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+	return (c);
+}
