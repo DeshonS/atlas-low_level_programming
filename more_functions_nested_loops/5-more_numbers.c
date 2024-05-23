fe12b821c8bc889f5ca4e8529a374d1ccc8d7a54
DeshonS <deshonjewell@gmail.com>
2024-05-23 23:21:57 +0000

    updated

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,20 +8,22 @@
 
 void more_numbers(void)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
-	while (i < 15)
+	for (i = 0; i < 10; i++)
 	{	
-		if (i < 10)
+		while (j < 15)
 		{
+			if (j > 9)
+			{
 			_putchar(i + '0');
 			i++;
-		}
-		else
-		{
-			_putchar(j + '0');
-			_putchar(i + '0');
+			}
+			else
+			{
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			i++;
 			j++;
 		}
