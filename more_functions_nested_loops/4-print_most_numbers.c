a2a2b8cc54b873daafba547b085b950357098997
DeshonS <deshonjewell@gmail.com>
2024-05-23 22:26:31 +0000

    first attempt at 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - opening of file
+ * Description: print numbers from 0 to 9 except 2 and 4
+ * Return: Always 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		if (c = 2 && c = 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c + '0');
+		}
+	c++;
+	}
+}
