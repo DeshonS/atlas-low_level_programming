da04c5fc4300ea59213f3ce9929ce239ab6a4b07
DeshonS <deshonjewell@gmail.com>
2024-05-23 21:47:21 +0000

    added description and changed symbol values in if statement

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ * _isupper - opening of file
+ * @c - char being checked for uppercase
+ * Return - 1 if uppercase, 0 if lower case
+ */
+
 int _isupper(int c)
 {
-	if (c > 'A' && c < 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 	return 1;
 	}
