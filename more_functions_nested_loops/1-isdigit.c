533a2d36a1d390260107e7970354bb4043597920
DeshonS <deshonjewell@gmail.com>
2024-05-23 21:54:28 +0000

    first attempt at 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - opening of file
+ * @c: int to be checked for digit status
+ * Return: 1 if digit, 0 if not a digit
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
