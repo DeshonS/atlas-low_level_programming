2f66571e60300154213074d9de0966772336680c
DeshonS <deshonjewell@gmail.com>
2024-05-23 22:10:09 +0000

    first attempt at 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - opening of file
+ * Description: prints numbers from 0 to 9 followed by a new line
+ * Return: always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int c = '0';
+
+	while (c <= 10)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
