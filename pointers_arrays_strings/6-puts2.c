7330b29834920d16de5fbeb419e05ca1ccb21048
DeshonS <deshonjewell@gmail.com>
2024-05-27 23:30:21 +0000

    first attempt at 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - opening of file
+ * @str: string being manipulated
+ * Return: Always 0 (Success)
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar('\n');
+}
