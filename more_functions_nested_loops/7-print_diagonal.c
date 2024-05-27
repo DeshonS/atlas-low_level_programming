c7f5c929156e4b08d55f14fd108243c0bdf7f374
DeshonS <deshonjewell@gmail.com>
2024-05-27 14:43:22 +0000

    update

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - opening of file
+ * @n: variable that indicates the number of spaces before line
+ * Return: Always 0 (Success)
+ */
+
+void print_diagonal(int n)
+{
+	if (n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int j = n;
+		int i = 0;
+
+		while (i < n)
+		{
+			_putchar(' ');
+			i++;
+		}
+		putchar('\\');
+		putchar('\n');
+		j++;
+	}
+}
+	
+
+
