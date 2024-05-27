c7db3a1f39e80d27cfcf9d37bcc673d7eb9358c8
DeshonS <deshonjewell@gmail.com>
2024-05-27 22:42:58 +0000

    first attempt at 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - opening of file
+ * @s: string to print in reverse
+ * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (int x = i - 1; x >= 0; x--)
+	{
+		_putchar(s[x]);
+	}
+	_putchar('\n');
+}
+
