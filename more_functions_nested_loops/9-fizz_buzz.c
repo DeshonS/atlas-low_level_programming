3b83de384562dd352a08aa5488f59881bd496323
DeshonS <deshonjewell@gmail.com>
2024-05-27 17:50:48 +0000

    first attempt at 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - opening of file
+ * Description: Prints Fizz Buzz algorithm
+ * Return: Always 0 (Success)
+ */
+
+int main()
+{
+	for (x = 0; x <= 100; x++)
+	{
+		if ((x % 3) == 0 && (x % 5) == 0)
+		{
+			printf("Fizzbuzz");
+		}
+		else if ((x % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((x % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf(x);
+		}
+	}
+}
