022a136203bb3e850318ab6c03b809c8b6423f5b
DeshonS <deshonjewell@gmail.com>
2024-05-27 23:07:19 +0000

    first attempt at 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - opening of file
+ * @s: string to be reversed
+ * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
