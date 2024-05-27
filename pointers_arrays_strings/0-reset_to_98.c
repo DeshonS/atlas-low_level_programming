94ffdbab36b8cf7924d21903618946cc37e34049
DeshonS <deshonjewell@gmail.com>
2024-05-27 22:04:45 +0000

    first attempt at 0;

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - opening of file
+ * @*n: pointer to variable to be changed
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
