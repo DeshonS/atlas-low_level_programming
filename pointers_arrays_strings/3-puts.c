740c8b4f6b9380b35c1090de21ea9e5fe4a84cfb
DeshonS <deshonjewell@gmail.com>
2024-05-27 22:19:21 +0000

    first attempt at 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - opening of file
+ * @str: string to be printed
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	printf(str);
+	printf("\n");
+}
