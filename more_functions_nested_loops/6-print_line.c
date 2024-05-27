7719c1d76945e406a392b17b6682167dc3047ea7
DeshonS <deshonjewell@gmail.com>
2024-05-27 11:47:27 +0000

    first attempt at 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - opening of file
+ * @n: variable that indicates number of lines to be printed
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+	if (n < 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		int j = 0;
+		while (j < n)
+		{
+			_putchar('_');
+			j++;
+		}
+	_putchar ('\n');
+	}
+}
