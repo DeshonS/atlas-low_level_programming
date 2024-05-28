ee4a4dc86bbeae145d45624235a6d5dfe73f1e29
DeshonS <deshonjewell@gmail.com>
2024-05-28 00:08:35 +0000

    updated

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - opening of file
+ * @str: variable being manipulated
+ * Return: Always 0 (Success)
+ */
+
+void puts_half(char str*)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if ((i % 2) != 0)
+	{
+		j = i % 2;
+		while (str[j] != '\0')
+		{
+			_putchar(str[j]);
+			j++;
+		}
+	}
+	_putchar('\n');
+}
