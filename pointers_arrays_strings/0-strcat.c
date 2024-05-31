aaf8508e96056bd3d9d73c014763f8c9c212aea1
DeshonS <deshonjewell@gmail.com>
2024-05-30 21:15:36 -0500

    updated

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,11 @@
 
 /**
  * _strcat - function that splices two strings together
+ * @dest: the first half of the string being spliced, also the final value
+ * @src: the second half of the string being spliced
+ * Return: Always 0 (Success)
 */
+
 char *_strcat(char *dest, char *src)
 {
 char *ptr = dest;
