3b0d9290c4bec78e02750fe68de97ce0448c3ab5
DeshonS <deshonjewell@gmail.com>
2024-05-30 21:11:07 -0500

    updated

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,19 +2,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-    char *ptr = dest;
+char *ptr = dest;
 
-    while(*ptr != '\0')
-    {
-        ptr++;
-    }
-    while (*src != '\0')
-    {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
-    *ptr = '\0';
+while(*ptr != '\0')
+{
+ptr++;
+}
+while (*src != '\0')
+{
+*ptr = *src;
 
-    return dest;
+ptr++;
+src++;
+}
+*ptr = '\0';
+
+return (dest);
 }
