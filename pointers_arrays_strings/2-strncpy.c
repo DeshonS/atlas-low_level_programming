b8debf27188e4de12ef366abff01a6c0ae6936cb
DeshonS <deshonjewell@gmail.com>
2024-06-01 16:21:46 -0500

    updated

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-while (*src != '\0')
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
-    i++;
+    dest[i] = src[i]
 }
-dest[i] = src[i];
 while (i < n)
 {
-    dest[i++] = '\0';
+    dest[i] = '\0';
+    i++;
 }
 return (dest);
 }
