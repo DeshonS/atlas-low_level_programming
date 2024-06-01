e136e08dab461ef23c0da390d47fcb0027098639
DeshonS <deshonjewell@gmail.com>
2024-06-01 16:56:30 -0500

    first attempt

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - opening of file
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Return: Always 0 (Success)
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+    int i;
+
+    while (s1[i] != '\0' && s1[i] != '\0')
+    {
+        if (s1[i] == s2[i])
+        {
+            i++;
+        }
+    }
+    return (s1[i] - s2[i]);
+}
