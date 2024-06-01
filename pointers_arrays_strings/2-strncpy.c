fe273b8ef11452d1ade7e80ab649faa7595dd44a
DeshonS <deshonjewell@gmail.com>
2024-06-01 16:16:42 -0500

    first attempt at 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _strncpy - opening of file
+ * @src: string to be copied
+ * @dest: string result of copying dest
+ * @n: number of charachters in the string
+ * Return: Always 0 (Success)
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int 
+while (*src != '\0')
+{
+    i++;
+}
+dest[i] = src[i];
+while (i < n)
+{
+    dest[i++] = '\0';
+}
+}
