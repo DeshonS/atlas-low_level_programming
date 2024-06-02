841beea0ce2fdbe1ce4393457e41368588031a61
DeshonS <deshonjewell@gmail.com>
2024-06-02 18:26:45 -0500

    first attempt at 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * cap_string - opening of file
+ * @x: string to be converted
+ * Return: Always 0 (Success)
+*/
+
+char *cap_string(char *x)
+{
+int i;
+
+while (x[i] != '\0')
+{
+if (x[i] = ' ' + 1)
+{
+x[i] = x[i] - 32;
+}
+}
+return (x);
+}
