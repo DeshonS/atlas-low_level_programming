d204a92f9185af7fbdb20a534aaff9e97f6fa41d
DeshonS <deshonjewell@gmail.com>
2024-06-02 17:38:55 -0500

    updated formatting for betty

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-    int i, t;
+int i, t;
 
-    for (i = 0; i < n / 2; i++)
-    {
-        t = a[i];
-        a[i] = a[n - i - 1];
-        a[n - i - 1] = t;
-    }
+for (i = 0; i < n / 2; i++)
+{
+t = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = t;
+}
 }
