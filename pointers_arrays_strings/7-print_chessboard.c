6f91b51b6f316305ac607122cd46eef17d728ab8
DeshonS <deshonjewell@gmail.com>
2024-06-03 19:28:41 -0500

    updated formatting

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,13 +8,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-    int x, y;
-    for (x = 0; x < 8; x++)
-    {
-        for (y = 0; y < 8; y++)
-        {
-            printf("%c", a[x][y]);
-        }
-    printf("\n");
-    }
+int x, y;
+for (x = 0; x < 8; x++)
+{
+for (y = 0; y < 8; y++)
+{
+printf("%c", a[x][y]);
+}
+printf("\n");
+}
 }
