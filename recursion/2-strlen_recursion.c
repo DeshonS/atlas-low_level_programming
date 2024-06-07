45fdb9ebb0592b17de29c3a5e97e07a8d8806919
DeshonS <deshonjewell@gmail.com>
2024-06-07 16:51:15 -0500

    updated

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,9 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-    if (*s)
+    if (*s == '\0')
     {
-        1 + _strlen_recursion(s + 1);
+        return 0;
     }
-    return (*s);
+    else
+    {
+    return (1 + _strlen_recursion(s + 1));
+    }
 }
