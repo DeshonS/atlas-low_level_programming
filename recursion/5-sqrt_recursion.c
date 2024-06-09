602ae4406ad32e9686af2c2665b6e0cd64566c70
DeshonS <deshonjewell@gmail.com>
2024-06-09 15:47:25 -0500

    updated documentation

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _sqrt_recursion - opening of file
  * @n: value that the code will return the square root of
- * @sqrt_o: function to check conditions of n and y and return result
+ * sqrt_o - function to check conditions of n and y and return result
  * @y: value that math result of x is compared to for return purposes
  * Return: Always 0 (Success)
 */
