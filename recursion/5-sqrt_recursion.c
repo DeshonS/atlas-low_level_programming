e88cb69ba61df092935ba8b54ae36d306f6d82a9
DeshonS <deshonjewell@gmail.com>
2024-06-09 15:27:38 -0500

    i feel confident about this plan

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - opening of file
+ * @n: value that the code will return the square root of
+ * Return: Always 0 (Success)
+*/
+
+int sqrt_o(int n, int y)
+{
+    if(n * n == y)
+    {
+        return (n);
+    }
+    if (n * n > y)
+    {
+        return (-1);
+    }
+    return (sqrt_o(n + 1, y));
+}
+
+int _sqrt_recursion (int n)
+{
+    return (sqrt_o(1, n));
+}
