e31f454b5dafb2d7294bc8e0c97919cc7bcfc53c
DeshonS <deshonjewell@gmail.com>
2024-06-10 06:23:59 -0500

    what even is PRIME

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * primecheck - opening of file
+ * @x: number to check for prime status
+ * @y: the dividing number to check x against
+ * Return: 1 for Prime, 0 if not
+*/
+
+int primecheck(int x, int y)
+{
+    if (x == y)
+    {
+        return (1);
+    }
+    if (x % y == 0)
+    {
+        return (0);
+    }
+    return (primecheck(x, y + 1));
+}
+
+int is_prime_number(int n)
+{
+    return (primecheck(n, 2));
+}
