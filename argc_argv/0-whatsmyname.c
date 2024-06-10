d40a6f7123f7b666e003dc9e1f0216820f89b881
DeshonS <deshonjewell@gmail.com>
2024-06-10 17:35:33 -0500

    first attempt

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - opening of file
+ * @argc: arg counter
+ * @argv: arg vector
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    printf("%s\n", argv[0]);
+    return (0);
+}
