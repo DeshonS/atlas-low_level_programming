3ca6588164dc2c4ce7f999ec4a3dd08b09dd4f20
DeshonS <deshonjewell@gmail.com>
2024-06-10 22:16:31 -0500

    updated

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,21 +12,21 @@
 int main(int argc, char *argv[])
 
 {
-    int x, y, add = 0;
+int x, y, add = 0;
 
-    for (x = 1; x < argc; x++)
+for (x = 1; x < argc; x++)
 
-    {
-        for (y = 0; argv[y][y] != '\0'; y++)
-        {
-            if (!isdigit(argv[x][y]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        add = add + atoi(argv[x]);
-    }
-    printf("%d\n", add);
-    return (0);
+{
+for (y = 0; argv[y][y] != '\0'; y++)
+{
+if (!isdigit(argv[x][y]))
+{
+printf("Error\n");
+return (1);
+}
+}
+add = add + atoi(argv[x]);
+}
+printf("%d\n", add);
+return (0);
 }
