8a193e97eab098a30c7c6fdb6485309e7dd021ef
DeshonS <deshonjewell@gmail.com>
2024-06-17 15:20:59 -0500

    done

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - opening of file
+ * @s1: first part of the string located in allocated memory
+ * @s2: second string of which n bytes will be used
+ * @n: number of bytes of s2 that will be used
+ * Return: Pointer to string consisting of s1 and n bytes of s2
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    int i, j;
+    char *a;
+    int x, y;
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
+    for (i = 0; s1[i] != '\0'; i++)
+    {
+        ;
+    }
+    for (j = 0; j < n; j++)
+    {
+        ;
+    }
+    a = malloc((i + j + 1) * sizeof(char));
+    if (a == NULL)
+    {
+        return (NULL);
+    }
+    for (x = 0; s1[x] != '\0'; x++)
+    {
+        a[x] = s1[x];
+    }
+    for (y = 0; y < n; y++)
+    {
+        a[x] = s2[y];
+        x++;
+    }
+    return (a);
+}
