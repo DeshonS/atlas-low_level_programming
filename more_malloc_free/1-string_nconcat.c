bdc5cfb6e08730d32331ed6beaf4227fc2c5a2af
DeshonS <deshonjewell@gmail.com>
2024-06-17 16:49:16 -0500

    backtracking

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,49 +2,48 @@
 #include <stdlib.h>
 
 /**
- * *string_nconcat - opening of file
- * @s1: first part of the string located in allocated memory
- * @s2: second string of which n bytes will be used
- * @n: number of bytes of s2 that will be used
- * Return: Pointer to string consisting of s1 and n bytes of s2
- */
+
+    *string_nconcat - opening of file
+    @s1: first part of the string located in allocated memory
+    @s2: second string of which n bytes will be used
+    @n: number of bytes of s2 that will be used
+    Return: Pointer to string consisting of s1 and n bytes of s2
+    */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i, j, z;
-    char *a;
-    int b = 0;
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-    for (i = 0; s1[i] != '\0'; i++)
-    {
-        ;
-    }
-    for (j = 0; s2[j] != '\0'; j++)
-    {
-        ;
-    }
-    a = malloc((i + j + 1) * sizeof(char));
-    if (a == NULL)
-    {
-        return (NULL);
-    }
-    for (z = 0; z < (i + j + 1); z++)
-    {
-    if (z < i)
-    {
-    a[z] = s1[z];
-    }
-    else if (z < n)
-    {
-    a[z] = s2[b++];
-    }
+int i, x;
+char *a;
+unsigned int j, y;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+;
+}
+for (j = 0; s2[j] != '\0' && j < n; j++)
+{
+;
+}
+a = malloc((i + j + 1) * sizeof(char));
+if (a == NULL)
+{
+return (NULL);
+}
+for (x = 0; s1[x] != '\0'; x++)
+{
+a[x] = s1[x];
+}
+for (y = 0; y < n; y++)
+{
+a[x] = s2[y];
+x++;
 }
 return (a);
 }
