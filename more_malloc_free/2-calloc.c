c61191f14cc2f5a05d60d28c0a4ced8effd0a8f5
DeshonS <deshonjewell@gmail.com>
2024-06-17 17:12:13 -0500

    done

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - opening of file
+ * @nmemb: number of elements in an array
+ * @size: number of bytes
+ * Return: Pointer to allocated memory, or NULL if error
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    int *a;
+    unsigned int i;
+
+    if (nmemb <= 0 || size <= 0)
+    {
+        return (NULL);
+    }
+
+    a = malloc(nmemb * size);
+    if (a == NULL)
+    {
+        return (NULL);
+    }
+    for (i = 0; i < nmemb; i++)
+    {
+        a[i] = 0;
+    }
+    return (a);
+}
