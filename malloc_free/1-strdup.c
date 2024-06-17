a8e689b5ed7d74ff2dbc70bb181524427fcaab7d
DeshonS <deshonjewell@gmail.com>
2024-06-17 07:30:13 -0500

    number 2

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - opening of file
+ * @str: the string that the return is a duplicate of
+ */
+
+char *_strdup(char *str)
+{
+unsigned int i;
+int x;
+char *a;
+
+if (str == NULL)
+{
+return (NULL);
+}
+for (x = 0; x != '\0'; x++)
+{
+;
+}
+a = malloc(x * sizeof(*a) + 1);
+
+if (a == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < x; i++)
+{
+a[i] = str[i];
+}
+return (a);
+}
