031308dc6ee9564bea57d90ad645effe1bb561a1
DeshonS <deshonjewell@gmail.com>
2024-06-21 19:33:08 -0500

    done

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+} my_dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner) {
+    if (d != NULL) {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
+}
