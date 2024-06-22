8deafde4d29f7ecdf099f136e6f9d013e91b3558
DeshonS <deshonjewell@gmail.com>
2024-06-21 19:52:03 -0500

    more formatting

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,8 +9,10 @@
  * @owner: dog owner
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner) {
-if (d != NULL) {
+void init_dog(struct dog *d, char *name, float age, char *owner) 
+{
+if (d != NULL) 
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
