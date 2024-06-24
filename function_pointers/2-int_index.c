29ac67f4599c9a47d134e10200a240c7aaf919a9
DeshonS <deshonjewell@gmail.com>
2024-06-24 17:01:08 -0500

    done

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - opening of file
+ * @array: int array
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: Always 0 (Success)
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    int i;
+
+    if (array == NULL || size <= 0 || cmp == NULL)
+    {
+        return (-1);
+    }
+    for (i = 0; i < size; i++)
+    {
+        if ((*cmp)(array[i]) != 0)
+        {
+            return (i);
+        }
+    }
+    return (-1);
+}
