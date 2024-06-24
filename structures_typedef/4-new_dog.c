f963b2aa6ac3c286e22c6d937da5b4aa63f62143
DeshonS <deshonjewell@gmail.com>
2024-06-24 13:43:21 -0500

    made adjustments to create the second set of variables

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,25 +6,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *x;
 x = malloc(sizeof(dog_t));
+x->name = malloc(strlen(name) + 1);
+x->owner = malloc(strlen(owner) + 1);
 
 if (x == NULL)
 {
 return (NULL);
 }
-if (name == NULL)
+if (x->name == NULL)
 {
 free(x);
-free(owner);
 return (NULL);
 }
-if (owner == NULL)
+strcpy(x->name, name);
+if (x->owner == NULL)
 {
 free(x);
-free(name);
+free(x->name);
 return (NULL);
 }
-x->name = name;
+strcpy(x->owner, owner);
 x->age = age;
-x->owner = owner;
 return (x);
 }
