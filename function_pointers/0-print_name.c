ab0a3a8a9cd5f92be482af722f5e09ff52ebd250
DeshonS <deshonjewell@gmail.com>
2024-06-24 15:55:40 -0500

    done

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pouinter to function
+ * Return: Always 0 (success)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+    if (name == NULL || f == NULL)
+    {
+        return;
+    }
+    (*f)(name);
+}
