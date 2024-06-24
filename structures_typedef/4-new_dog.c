7ba21f8c953b16ca9bbc4dbaaea9441527872ee1
DeshonS <deshonjewell@gmail.com>
2024-06-24 13:54:45 -0500

    updated

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,42 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+size_t string_length(const char *str)
+{
+size_t length = 0;
+while (str[length] != '\0')
+{
+length++;
+}
+return length;
+}
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *x;
-int i, j;
 x = malloc(sizeof(dog_t));
-x->name = malloc(i + 1);
-x->owner = malloc(j + 1);
-
-
-for (i = 0; i != name['\0']; i++)
-{
-    ;
-}
-for (j = 0; j != owner['\0']; j++)
-{
-    ;
-}
 if (x == NULL)
 {
-return (NULL);
+return NULL;
 }
+
+size_t name_len = string_length(name);
+x->name = malloc(name_len + 1);
 if (x->name == NULL)
 {
 free(x);
-return (NULL);
+return NULL;
 }
-strcpy(x->name, name);
+for (size_t i = 0; i <= name_len; i++)
+{
+x->name[i] = name[i];
+}
+
+size_t owner_len = string_length(owner);
+x->owner = malloc(owner_len + 1);
 if (x->owner == NULL)
 {
-free(x);
 free(x->name);
-return (NULL);
+free(x);
+return NULL;
 }
-strcpy(x->owner, owner);
+for (size_t i = 0; i <= owner_len; i++)
+{
+x->owner[i] = owner[i];
+}
+
 x->age = age;
-return (x);
+
+return x;
 }
