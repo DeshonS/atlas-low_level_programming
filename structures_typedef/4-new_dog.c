e0219b5d041b388af2aeefd91b703adceb8ef3ff
DeshonS <deshonjewell@gmail.com>
2024-06-24 14:12:01 -0500

    updated

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,51 +2,47 @@
 #include <stdlib.h>
 #include "dog.h"
 
-size_t string_length(const char *str)
-{
-size_t length = 0;
-while (str[length] != '\0')
-{
-length++;
-}
-return length;
-}
+/**
+ * dog_t - name of typedef
+ * *new_dog - name of struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: new credentials for new_dog
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *x;
-x = malloc(sizeof(dog_t));
-if (x == NULL)
-{
-return NULL;
-}
+    char str;
+    int i, j;
+    dog_t *x;
+    x = malloc(sizeof(dog_t));
+    x->name = malloc(sizeof(name) + 1);
+    x->owner = malloc(sizeof(owner) + 1);
 
-size_t name_len = string_length(name);
-x->name = malloc(name_len + 1);
-if (x->name == NULL)
-{
-free(x);
-return NULL;
-}
-for (size_t i = 0; i <= name_len; i++)
-{
-x->name[i] = name[i];
-}
-
-size_t owner_len = string_length(owner);
-x->owner = malloc(owner_len + 1);
-if (x->owner == NULL)
-{
-free(x->name);
-free(x);
-return NULL;
-}
-for (size_t i = 0; i <= owner_len; i++)
-{
-x->owner[i] = owner[i];
-}
-
-x->age = age;
-
-return x;
+    if (x == NULL)
+    {
+        return (NULL);
+    }
+    if (x->name == NULL)
+    {
+        free(x);
+        return (NULL);
+    }
+    for (i = 0; name[i] != '\0'; i++)
+    {
+        x->name[i] = name[i];
+    }
+    if(x->owner == NULL)
+    {
+        free(x);
+        free(x->name);
+        return (NULL);
+    }
+    for (j = 0; owner[j] != '\0'; j++)
+    {
+        x->owner[j] = owner[j];
+    }
+    x->age = age;
+    return (x);
 }
