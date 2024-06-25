535f76c7ac753549affcc99e794f84f518a064b0
DeshonS <deshonjewell@gmail.com>
2024-06-24 21:08:09 -0500

    done

---
+++ function_pointers/3-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - opening of file
+ * @argc: argument counter
+ * argv: arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+    int x;
+
+    if (argc != 4)
+    {
+        return(NULL);
+    }
+    x = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+    printf("%d\n", x);
+    return (0);
+}
