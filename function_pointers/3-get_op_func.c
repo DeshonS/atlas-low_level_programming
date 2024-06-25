f1df3768d39f979fc551d0f7f0ba449dbfbe8c05
DeshonS <deshonjewell@gmail.com>
2024-06-24 20:57:55 -0500

    updated

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,21 +10,21 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"%", op_mod},
-        {"/", op_div},
-        {NULL, NULL}
-    };
-    int i;
-    for (i = 0; ops[i].op != NULL; i++)
-    {
-        if(ops[i].op[0] == s[0] && s[1] == '\0')
-        {
-            return ops[i].f;
-        }  
-    }
-    return (NULL);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"%", op_mod},
+{"/", op_div},
+{NULL, NULL}
+};
+int i;
+for (i = 0; ops[i].op != NULL; i++)
+{
+if(ops[i].op[0] == s[0] && s[1] == '\0')
+{
+return ops[i].f;
+}  
+}
+return (NULL);
 }
