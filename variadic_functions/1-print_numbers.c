b1c6612e6b1f8195778f6543def1f1c01d0f19cd
DeshonS <deshonjewell@gmail.com>
2024-06-25 09:21:59 -0500

    updated

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
             continue;
         }
         printf("%d", va_arg(args, int));
-        if (i != n)
+        if (i != n - 1)
         {
         printf("%s", separator);
         }
