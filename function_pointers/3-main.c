7193a035b90534a7e5557eebf63a5218c2c754ba
DeshonS <deshonjewell@gmail.com>
2024-06-24 21:10:37 -0500

    updated

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,6 +13,10 @@ int main(int argc, char **argv)
 {
     int x;
 
+    if (argc != 4)
+    {
+        printf("Invalid Syntax");
+    }
     x = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
     printf("%d\n", x);
     return (0);
