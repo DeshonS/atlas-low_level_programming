9ef0e05ff9d7261f9d47abf031015cf92cfde105
DeshonS <deshonjewell@gmail.com>
2024-06-24 20:57:40 -0500

    done

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <3-calc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - opening of file
+ * @s: charachter to look for to determine function
+ * Return: Always 0 (Success)
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"%", op_mod},
+        {"/", op_div},
+        {NULL, NULL}
+    };
+    int i;
+    for (i = 0; ops[i].op != NULL; i++)
+    {
+        if(ops[i].op[0] == s[0] && s[1] == '\0')
+        {
+            return ops[i].f;
+        }  
+    }
+    return (NULL);
+}
