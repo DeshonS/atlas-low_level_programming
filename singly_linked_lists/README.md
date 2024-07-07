e674828b6fa201f910b3f5c26bbcd57225812f23
DeshonS <deshonjewell@gmail.com>
2024-07-07 12:21:42 -0500

    first go (Australia #1)

---
+++ singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Linked lists are amazing!
---
+++ singly_linked_lists/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+typedef struct list
+{
+    char *str;
+    int len;
+    struct list *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif
