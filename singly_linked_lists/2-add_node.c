0e6dd838916e2deffbe9b18a83d7dbde65e0ac28
DeshonS <deshonjewell@gmail.com>
2024-07-07 15:57:30 -0500

    updated

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,12 +12,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new = malloc(sizeof(list_t));
+char newstr = new->str;
 
 if (new == NULL)
 {
 return (NULL);
 }
-new->str = str;
+new->str = strdup(str);
 new->next = *head;
 *head = new;
 return (*head);
