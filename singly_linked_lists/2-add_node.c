8038fbb4e4ab2bc3f4300aab21cf6f960ebbb893
DeshonS <deshonjewell@gmail.com>
2024-07-07 15:50:39 -0500

    done

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - opening of file
+ * @head: pointer to start of list
+ * @str: pointer to string
+ * Return: Pointer to address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new = malloc(sizeof(list_t));
+
+if (new == NULL)
+{
+return (NULL);
+}
+new->str = str;
+new->next = *head;
+*head = new;
+return (*head);
+}
