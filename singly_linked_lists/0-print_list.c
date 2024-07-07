d20f227d7515047d64645f8c23b550e4cc15d393
DeshonS <deshonjewell@gmail.com>
2024-07-07 12:30:11 -0500

    updated

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,14 +2,15 @@
 #include "lists.h"
 
 /**
- * print_list - opening of file
- * @h: pointer to a list
- * Return: Number of nodes
- */
+* print_list - opening of file
+* @h: pointer to a list
+* Return: Number of nodes
+*/
 
 size_t print_list(const list_t *h)
 {
 int i = 0;
+
 while (h != NULL)
 {
 if(h->str == NULL)
