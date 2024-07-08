45c63f2dea13096722c476ea1747caaf06836f52
DeshonS <deshonjewell@gmail.com>
2024-07-08 09:02:46 -0500

    done

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end - opening of file
+ * @head: pointer to start of list
+ * @str: pointer to data
+ * Return: Address of new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+int i;
+list_t *ptr;
+
+list_t *new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+{
+new->str = strdup(str);
+new->len = i;
+}
+new->next = NULL;
+if (*head != NULL)
+{
+ptr = *head;
+while (ptr->next != NULL)
+{
+ptr = ptr->next;
+}
+ptr->next = new;
+}
+else
+{
+*head = new;
+}
+return (new);
+}
