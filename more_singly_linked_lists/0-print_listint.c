68360338387078e5f76fc7e66dee8a611dca8a6d
DeshonS <deshonjewell@gmail.com>
2024-07-08 10:07:47 -0500

    ok i had to do some more editiong than id expected

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -9,18 +9,11 @@
 
 size_t print_listint(const listint_t *h)
 {
-int i = 0;
+size_t i = 0;
 
 while (h != NULL)
 {
-if (h->str == NULL)
-{
 printf("[0] (nil)\n");
-}
-else
-{
-printf("[%d] %s\n", h->len, h->str);
-}
 h = h->next;
 i++;
 }
