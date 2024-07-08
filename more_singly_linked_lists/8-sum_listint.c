0be72e318d9e64d1334c7c8c4bd7e477aab8ccc0
DeshonS <deshonjewell@gmail.com>
2024-07-08 14:52:14 -0500

    done

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - opening of file
+ * @head: pointer to start of list
+ * Return: sum of all data of a list
+ */
+
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+
+while (head != NULL)
+{
+sum = sum + head;
+head = head->next;
+}
+return (sum);
+}
