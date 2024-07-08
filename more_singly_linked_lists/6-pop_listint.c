1910446ef5aaac45f17e3e387e080a0b854ad168
DeshonS <deshonjewell@gmail.com>
2024-07-08 12:54:39 -0500

    done

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - opening of file
+ * @head: double pointer to start of list
+ * Return: data of head node
+ */
+
+int pop_listint(listint_t **head)
+{
+int i = 0;
+listint_t *new;
+
+if (*head == NULL)
+{
+return (0);
+}
+i = (*head)->n;
+new = *head;
+*head = (*head)->next;
+free(new);
+return(i);
+}
