bd9e62fc67dae39111cd9f2341835e4f1950f5cf
DeshonS <deshonjewell@gmail.com>
2024-07-08 10:15:56 -0500

    done

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len - opening of file
+ * @h: pointer to list
+ * Return: Number of nodes in a list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+    int i;
+
+    while(h != NULL)
+    {
+        h = h->next;
+        i++;
+    }
+    return (i);
+}
