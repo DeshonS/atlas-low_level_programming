644cb8bc2d8e346b3e5d45a4d4c0312e99dae82f
DeshonS <deshonjewell@gmail.com>
2024-07-08 10:22:19 -0500

    done

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - opening of file
+ * @head: pointer to start of list
+ * @n: data
+ * Return: location of memory for new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    int i;
+    listint_t *new;
+
+    new = malloc(sizeof(listint_t));
+
+    if (new == NULL)
+    {
+        return (NULL);
+    }
+    new->n = n;
+    new->next = *head;
+    *head = new;
+    return (*head);
+}
