6a750d26eb5dfbc5bca81cff1a164a89c8ab063c
DeshonS <deshonjewell@gmail.com>
2024-07-08 09:24:23 -0500

    done

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - opening of file
+ * @head: pointer to start of list
+ * Return: None
+ */
+
+void free_list(list_t *head)
+{
+list_t *new;
+
+while ((new = head) != NULL)
+{
+head = head->next;
+free(new->str);
+free(new);
+}
+}
