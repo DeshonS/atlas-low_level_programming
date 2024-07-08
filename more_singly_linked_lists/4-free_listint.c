dd5e5dab0064b51a7198ee35fb4e977443772296
DeshonS <deshonjewell@gmail.com>
2024-07-08 12:08:57 -0500

    form atted

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -10,11 +10,11 @@
 
 void free_listint(listint_t *head)
 {
-    listint_t *new;
-    while (head != NULL)
-    {
-        new = head->next;
-        free(head);
-        head = new;
-    }
+listint_t *new;
+while (head != NULL)
+{
+new = head->next;
+free(head);
+head = new;
+}
 }
