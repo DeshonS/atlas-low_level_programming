8a70e4ad74d1195ac528fb21a5044e40499c74b8
DeshonS <deshonjewell@gmail.com>
2024-07-07 21:08:58 -0500

    updated

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,14 +13,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new = malloc(sizeof(list_t));
+int i;
 
 if (new == NULL)
 {
 return (NULL);
 }
 new->str = strdup(str);
+for (i = 0; str[i] != '\0'; i++)
+{
+    ;
+}
+new->len = i;
 new->next = *head;
 *head = new;
-printf("[%d] %s", new->len, new->str);
-return (0);
+return (new);
 }
