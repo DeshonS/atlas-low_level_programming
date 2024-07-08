c453ac1cae57d96fdf3cb93e2ba6e67af7edf7ed
DeshonS <deshonjewell@gmail.com>
2024-07-08 14:48:06 -0500

    done

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - opening of file
+ * @head: pointer to start of list
+ * @index: index of node
+ * Return: nth node, or NULL if node does not exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+int i;
+
+for (i = 0; head != NULL; i++)
+{
+if (i == index)
+{
+return (head);
+}
+head = head->next;
+}
+return (NULL);
+}
