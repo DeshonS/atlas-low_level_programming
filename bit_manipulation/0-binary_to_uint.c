dbcbeddc41d27b296124611f770d514649cc16fd
DeshonS <deshonjewell@gmail.com>
2024-07-08 17:43:48 -0500

    starting project 3

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_unit - opening of file
+ * @b: binary code to convert to int
+ * Return: Int conversion of binary code
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+int i;
+unsigned int newint = 0;
+if (b == NULL)
+{
+return (0);
+}
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+{
+return (0);
+}
+newint << 1;
+if (b == '1')
+{
+newint = newint + 1;
+}
+}
+return newint;
+}
