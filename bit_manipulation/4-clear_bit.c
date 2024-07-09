cbf05e27c055ce42aca111184a87543bca2ab018
DeshonS <deshonjewell@gmail.com>
2024-07-08 19:48:58 -0500

    done

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * clear_bit - opening of file
+ * @n: data
+ * @index: index to change
+ * Return: 1 for success, -1 for failure
+ */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= (sizeof(n) * 8))
+if (index > (sizeof(n) * 8))
 {
 return (-1);
 }
-*n |= 0 << index;
+*n &= ~(0 << index);
 return (1);
 }
