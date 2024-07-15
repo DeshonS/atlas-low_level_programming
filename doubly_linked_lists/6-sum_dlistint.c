b638369dfafd623b66f9b40ca0be7f496b4c4179
DeshonS <deshonjewell@gmail.com>
2024-07-15 07:34:04 -0500

    done

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - opening of file
+ * @head: pointer to start of list
+ * Return: Sum of list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+
+if (!head)
+{
+return (0);
+}
+while (head != NULL)
+{
+sum = sum + head;
+head = head->next;
+}
+return (sum);
+}
