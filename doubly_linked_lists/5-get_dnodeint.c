ca604235514a277cb83b90c7473b19bcc5aca603
DeshonS <deshonjewell@gmail.com>
2024-07-15 07:30:02 -0500

    done

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - opening of file
+ * @head: pointer to start of list
+ * @index: location of data needing to be accessed
+ * Return: data at index or NULL if failed
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+dlistint_t *new;
+int i;
+
+for (i = 0; head != NULL; i++)
+{
+if (i == index)
+{
+return (head);
+}
+head = head->next;
+}
+return (NULL);
+}
