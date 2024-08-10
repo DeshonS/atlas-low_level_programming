8de2fa6ccb4a0aab38e84ef63048d19bb9fd71d6
DeshonS <deshonjewell@gmail.com>
2024-08-10 06:24:33 -0500

    changed up my printf

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,7 +9,7 @@ int linear_search(int *array, size_t size, int value)
     }
     for (i = 0; i < size; i++)
     {
-        printf("%d", value);
+        printf("Value checked array[%d] = [%d]", i, array[i]);
         if (array[i] == value)
         {
             return (i);
