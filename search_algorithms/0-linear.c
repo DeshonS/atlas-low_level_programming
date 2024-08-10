852fe41a8ffeb562e3c0140b5a56786125e9d663
DeshonS <deshonjewell@gmail.com>
2024-08-10 06:27:56 -0500

    added documentation and formatted

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,19 +1,27 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - opening of file
+ * @array: pointer to array
+ * @size: size of the array
+ * @value: value for comparison
+ * Return: first indexed location of value, or -1 for errors
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
-    long unsigned int i;
-    if (array == NULL)
-    {
-        return (-1);
-    }
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-        {
-            return (i);
-        }
-    }
-    return (-1);
+long unsigned int i;
+if (array == NULL)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+{
+return (i);
+}
+}
+return (-1);
 }
