b2099d23bd13822029ca3ea9102bd060b050157f
DeshonS <deshonjewell@gmail.com>
2024-08-10 06:51:54 -0500

    added prototypes for binary_search and print_array

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,5 +6,7 @@
 #include <string.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t low, size_t high);
 
 #endif
